52e4fff1e3dc50b9f4a89a7a32a470c169241d95
Vishal Khatri <vishalsarkar2050@gmail.com>
2019-11-11 21:05:08 +0800

    question 1

---
+++ src/com/singtel/Solution.java
@@ -0,0 +1,10 @@
+package com.singtel;
+
+public class Solution {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}

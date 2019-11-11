ea9c0787731cf06a945fe0a702c227f151c6cdf2
Vishal Khatri <vishalsarkar2050@gmail.com>
2019-11-11 21:39:12 +0800

    question E Counting animals

--- src/com/singtel/Solution.java
+++ src/com/singtel/Solution.java
@@ -3,10 +3,34 @@ package com.singtel;
 public class Solution {
 
     public static void main(String[] args) {
-        Bird bird = new Bird();
-        bird.walk();
-        bird.fly();
-        bird.sing();
+        Animal[] animals = new Animal[]{
+                new Bird(),
+                new Duck(),
+                new Chicken(false),
+                new Chicken(true),
+                new Parrot(new Duck()),
+                new Fish(),
+                new Shark(),
+                new Catterpiller(),
+                new Clownfish(),
+                new ButterFly()
+        };
+
+        int singableAnimal = 0;
+        int walkableAnimal = 0;
+        int swimmableAnimal = 0;
+        int flyableAnimal = 0;
+
+        for(Animal animal: animals){
+            if(animal instanceof Singable) singableAnimal++;
+            if(animal instanceof Walkable) walkableAnimal++;
+            if(animal instanceof Swimmable) swimmableAnimal++;
+            if(animal instanceof Flyable) flyableAnimal++;
+        }
+
+        System.out.printf("Singable: %d, Walkable: %d, Swimmable: %d, Flyable: %d", singableAnimal, walkableAnimal, swimmableAnimal, flyableAnimal);
+
+
     }
 
 }
